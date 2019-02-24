31eebf429413863025b971402f8c79823929c427
Steven Lam <lam.steven7@gmail.com>
2019-02-24 14:41:14 -0500

    testing with hardcoded pdf url

--- src/background.js
+++ src/background.js
@@ -10,6 +10,9 @@ chrome.runtime.onMessage.addListener(msg => {
 
   changeBasePath(BASE_PATH + msg.courseName);
   chrome.downloads.download({
-    url: msg.url
+    // url: msg.url
+    url:
+      // "https://moodle.polymtl.ca/pluginfile.php/59104/mod_resource/content/6/htmlgen/plan_de_cours_INF4705"
+      "https://moodle.polymtl.ca/mod/resource/view.php?id=29493&redirect=1"
   });
 });
--- src/content.js
+++ src/content.js
@@ -1,8 +1,10 @@
 console.log("I AM MARCUS!");
 
 const resources = document.getElementsByClassName("activity resource");
+console.log(resources[0]);
 const url =
   resources[0].children[0].children[0].children[1].children[0].children[0].href;
+console.log(url);
 
 const courseName = document.getElementsByClassName("page-header-headings")[0]
   .children[0].innerHTML;
