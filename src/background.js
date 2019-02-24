66a608d9400bf605d8c402b2d4304e86efc28b89
Steven Truong <tsteven@live.ca>
2019-02-24 16:10:29 -0500

    removed useless comment

--- src/background.js
+++ src/background.js
@@ -15,10 +15,7 @@ chrome.runtime.onMessage.addListener(msg => {
   prefix = BASE_PATH + msg.courseName + "/";
   suffix = msg.extension;
   chrome.downloads.download({
-    // url: msg.url
     url: msg.url
-    // "https://moodle.polymtl.ca/pluginfile.php/59104/mod_resource/content/6/htmlgen/plan_de_cours_INF4705"
-    //"https://moodle.polymtl.ca/mod/resource/view.php?id=29493&redirect=1"
   });
 });
 
