8706e83cc3f5df66ebf7cdb4a7f7617c1f37fe6b
Steven Truong <tsteven@live.ca>
2019-02-24 12:36:28 -0500

    adding a images changing extension

---
+++ change-image-extension/background.js
@@ -0,0 +1,12 @@
+console.log("background running"); 
+
+chrome.browserAction.onClicked.addListener(buttonClicked);
+
+function buttonClicked(tab){
+
+    let msg = {
+        txt: "hello"
+    }
+    chrome.tabs.sendMessage(tab.id, msg)
+    console.log(tab);
+}
