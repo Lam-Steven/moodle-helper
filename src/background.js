96549d61d3caccb2c3c5dc41b17e44c858792dbd
Olivia You-Tuon <oyoutuon@gmail.com>
2019-02-24 18:12:44 -0500

    Add check if has dot in filename, empty suffix

--- src/background.js
+++ src/background.js
@@ -3,15 +3,14 @@ const BASE_PATH = "moodle-helper/";
 let prefix = "";
 let suffix = "";
 chrome.downloads.onDeterminingFilename.addListener((item, suggest) => {
-  const newfilename = prefix + item.filename + suffix;
-  console.log(newfilename);
+  if(item.filename.indexOf(".") > -1) {
+    suffix = "";
+  }
   suggest({ filename: prefix + item.filename + suffix });
 })
 
 
-
 chrome.runtime.onMessage.addListener(msg => {
-  console.log(msg.url);
   prefix = BASE_PATH + msg.courseName + "/";
   suffix = msg.extension;
   chrome.downloads.download({
