6bc5541c7aafeed7f91a2aaf3bc0d722c4f40f59
Olivia You-Tuon <oyoutuon@gmail.com>
2019-03-03 21:04:40 -0500

    Fetch html code from item url to get the right downloadable url

--- src/background.js
+++ src/background.js
@@ -12,9 +12,14 @@ chrome.runtime.onMessage.addListener(msg => {
 })
 
 chrome.downloads.onDeterminingFilename.addListener((item, suggest) => {
+  if(item.filename == "view.html") {
+    downloadFromFrame(item);
+    chrome.downloads.cancel(item.id);
+  }
   const suffix = hasExtension(item.filename) ? '' : extension
   suggest({ filename: prefix + item.filename + suffix })
 })
+
 
 function downloadFiles(resources) {
   Array.prototype.forEach.call(resources, downloadFile)
@@ -29,4 +34,21 @@ function downloadFile(resource) {
 
 function hasExtension(fileName) {
   return fileName.indexOf('.') > -1
+}
+
+function downloadFromFrame(item) {
+  fetch(item.url).then((data) => {
+    return data.text();
+  })
+  .then( (text) => {
+    const anchor = "frame src=";
+    const offset = anchor.length + 1;
+    const first = text.indexOf(anchor, text.indexOf(anchor) + offset);
+    
+    if (first > -1) {
+      const second = text.indexOf("\"", first + offset);
+      const url = text.substring(first + offset, second);
+      downloadFile({extension: '.pdf', url: url});
+    }
+  });
 }
