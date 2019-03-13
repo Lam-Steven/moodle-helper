19be9bb0b609f00fb6d648834213f8fa9c519aba
hnspn <bobmatt911@gmail.com>
2019-03-13 14:26:47 -0400

    changed school url source to location href

--- src/content.js
+++ src/content.js
@@ -1,10 +1,9 @@
-const ILLEGAL_CHARACTERS = /[*?"<>|\\\/:]/g;
+const ILLEGAL_CHARACTERS = /[*?"<>|\\\/:]/g
 
 chrome.runtime.onMessage.addListener(msg => {
   if (msg.recipient == 'content' && msg.start) {
-
     const sections = document.getElementsByClassName('section main clearfix')
-    let allResources = [];
+    let allResources = []
 
     Array.prototype.forEach.call(sections, s => {
       if (!isHidden(s)) {
@@ -13,26 +12,30 @@ chrome.runtime.onMessage.addListener(msg => {
 
         Array.prototype.forEach.call(resources, resource => {
           if (isActivityResource(resource)) {
-            const anchorTag = resource.children[0].children[0].children[1].children[0].children[0]
+            const anchorTag =
+              resource.children[0].children[0].children[1].children[0]
+                .children[0]
             const nameTag = anchorTag.children[1]
             const iconTag = anchorTag.children[0]
             resourcesArray.push({
               iconUrl: iconTag.src,
-              url: isWindowOpen(anchorTag) ? getDownloadURL(anchorTag) : anchorTag.href,
-              name: nameTag.innerHTML
+              url: isWindowOpen(anchorTag)
+                ? getDownloadURL(anchorTag)
+                : anchorTag.href,
+              name: nameTag.innerHTML,
             })
           }
         })
         allResources.push({
           section: s.getAttribute('aria-label'),
-          resources: resourcesArray
+          resources: resourcesArray,
         })
       }
     })
     chrome.runtime.sendMessage({
       recipient: 'popup',
       resources: allResources,
-      courseName: getCourseName()
+      courseName: getCourseName(),
     })
   }
 })
@@ -58,35 +61,36 @@ function isWindowOpen(a) {
 function getDownloadURL(anchorTag) {
   const s = anchorTag.getAttribute('onclick')
   const first = s.indexOf("'") + 1
-  const second = s.indexOf("'", first) 
+  const second = s.indexOf("'", first)
   return s.substring(first, second)
 }
 
 function getSchool() {
-  const schoolURL = document.getElementsByTagName('link')[0].getAttribute('href');
-  const end = schoolURL.indexOf(".ca") 
+  const schoolURL = window.location.href
+  const end = schoolURL.indexOf('.ca')
   const start = schoolURL.lastIndexOf('.', end - 1) + 1
   return schoolURL.substring(start, end)
 }
 
 function getResources(s) {
-  switch(getSchool()) {
-    case('polymtl') :
+  switch (getSchool()) {
+    case 'polymtl':
       return s.children[3].children[3].children
-    case('uqam') :
+    case 'uqam':
       return s.children[2].children[1].children[0].children
   }
 }
 
 function getCourseName() {
-  let courseName = '';
+  let courseName = ''
   switch (getSchool()) {
-    case('polymtl') :
-      courseName = document.getElementsByClassName('page-header-headings')[0].children[0].innerHTML
-      break;
-    case('uqam') :
-      courseName = document.getElementsByClassName('titre-cours')[0].innerHTML;
-      break;
+    case 'polymtl':
+      courseName = document.getElementsByClassName('page-header-headings')[0]
+        .children[0].innerHTML
+      break
+    case 'uqam':
+      courseName = document.getElementsByClassName('titre-cours')[0].innerHTML
+      break
   }
-  return courseName.replace(ILLEGAL_CHARACTERS, '').trim();
+  return courseName.replace(ILLEGAL_CHARACTERS, '').trim()
 }
