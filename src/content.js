c6f0b5d30763c1c033636bcb4ed915a98d6999dc
hnspn <bobmatt911@gmail.com>
2019-03-13 14:45:08 -0400

    added semicolons

--- src/content.js
+++ src/content.js
@@ -2,89 +2,95 @@ const ILLEGAL_CHARACTERS = /[*?"<>|\\\/:]/g;
 
 chrome.runtime.onMessage.addListener(msg => {
   if (msg.recipient == 'content' && msg.start) {
-
-    const sections = document.getElementsByClassName('section main clearfix')
+    const sections = document.getElementsByClassName('section main clearfix');
     let allResources = [];
 
     Array.prototype.forEach.call(sections, s => {
       if (!isHidden(s)) {
-        const resources = getResources(s)
-        const resourcesArray = []
+        const resources = getResources(s);
+        const resourcesArray = [];
 
         Array.prototype.forEach.call(resources, resource => {
           if (isActivityResource(resource)) {
-            const anchorTag = resource.children[0].children[0].children[1].children[0].children[0]
-            const nameTag = anchorTag.children[1]
-            const iconTag = anchorTag.children[0]
+            const anchorTag =
+              resource.children[0].children[0].children[1].children[0]
+                .children[0];
+            const nameTag = anchorTag.children[1];
+            const iconTag = anchorTag.children[0];
             resourcesArray.push({
               iconUrl: iconTag.src,
-              url: isWindowOpen(anchorTag) ? getDownloadURL(anchorTag) : anchorTag.href,
-              name: nameTag.innerHTML
-            })
+              url: isWindowOpen(anchorTag)
+                ? getDownloadURL(anchorTag)
+                : anchorTag.href,
+              name: nameTag.innerHTML,
+            });
           }
-        })
+        });
         allResources.push({
           section: s.getAttribute('aria-label'),
-          resources: resourcesArray
-        })
+          resources: resourcesArray,
+        });
       }
-    })
+    });
     chrome.runtime.sendMessage({
       recipient: 'popup',
       resources: allResources,
-      courseName: getCourseName()
-    })
+      courseName: getCourseName(),
+    });
   }
-})
+});
 
 function isHidden(s) {
-  return s.getAttribute('class') == 'section main clearfix hidden'
+  return s.getAttribute('class') == 'section main clearfix hidden';
 }
 
 function isActivityResource(r) {
   if (r.hasAttribute('class')) {
-    return r.getAttribute('class').includes('activity resource')
+    return r.getAttribute('class').includes('activity resource');
   }
-  return false
+  return false;
 }
 
 function isWindowOpen(a) {
   return (
     a.hasAttribute('onclick') &&
     a.getAttribute('onclick').indexOf('window.open') > -1
-  )
+  );
 }
 
 function getDownloadURL(anchorTag) {
-  const s = anchorTag.getAttribute('onclick')
-  const first = s.indexOf("'") + 1
-  const second = s.indexOf("'", first) 
-  return s.substring(first, second)
+  const s = anchorTag.getAttribute('onclick');
+  const first = s.indexOf("'") + 1;
+  const second = s.indexOf("'", first);
+  return s.substring(first, second);
 }
 
 function getSchool() {
-  const schoolURL = document.getElementsByTagName('link')[0].getAttribute('href');
-  const end = schoolURL.indexOf(".ca") 
-  const start = schoolURL.lastIndexOf('.', end - 1) + 1
-  return schoolURL.substring(start, end)
+  const schoolURL = document
+    .getElementsByTagName('link')[0]
+    .getAttribute('href');
+  const end = schoolURL.indexOf('.ca');
+  const start = schoolURL.lastIndexOf('.', end - 1) + 1;
+  return schoolURL.substring(start, end);
 }
 
 function getResources(s) {
-  switch(getSchool()) {
-    case('polymtl') :
-      return s.children[3].children[3].children
-    case('uqam') :
-      return s.children[2].children[1].children[0].children
+  switch (getSchool()) {
+    case 'polymtl':
+      return s.children[3].children[3].children;
+    case 'uqam':
+      return s.children[2].children[1].children[0].children;
   }
 }
 
 function getCourseName() {
   let courseName = '';
   switch (getSchool()) {
-    case('polymtl') :
-      courseName = document.getElementsByClassName('page-header-headings')[0].children[0].innerHTML
+    case 'polymtl':
+      courseName = document.getElementsByClassName('page-header-headings')[0]
+        .children[0].innerHTML;
       break;
-    case('uqam') :
+    case 'uqam':
       courseName = document.getElementsByClassName('titre-cours')[0].innerHTML;
       break;
   }
--- src/popup.js
+++ src/popup.js
@@ -1,69 +1,68 @@
 chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-  chrome.tabs.sendMessage(tabs[0].id, { recipient: 'content', start: true })
-})
+  chrome.tabs.sendMessage(tabs[0].id, { recipient: 'content', start: true });
+});
 
 chrome.runtime.onMessage.addListener(msg => {
   if (msg.recipient == 'popup') {
     const inputs = [];
     let counter = 0;
-    const title = document.createElement('H1')
-    title.innerHTML = msg.courseName
-    document.body.appendChild(title)
+    const title = document.createElement('H1');
+    title.innerHTML = msg.courseName;
+    document.body.appendChild(title);
 
-    const checkBoxCounter = document.createElement('H2')
-    checkBoxCounter.innerHTML = 'Nombre de fichiers: ' + counter
-    document.body.appendChild(checkBoxCounter)
+    const checkBoxCounter = document.createElement('H2');
+    checkBoxCounter.innerHTML = 'Nombre de fichiers: ' + counter;
+    document.body.appendChild(checkBoxCounter);
 
-    const selectAllCheckbox = document.createElement('INPUT')
-    const selectAllCheckboxText = document.createElement('H2')
-    selectAllCheckboxText.innerHTML = 'Check all'
-    selectAllCheckboxText.style.display = 'inline'
-    selectAllCheckbox.addEventListener('change', function(){
-        selectAllCheckboxes(inputs, this.checked)       
-    })
-    
-    selectAllCheckbox.setAttribute("type", "checkbox");
-    document.body.appendChild(selectAllCheckbox)
-    document.body.appendChild(selectAllCheckboxText)
-    
-    const allResources = []
+    const selectAllCheckbox = document.createElement('INPUT');
+    const selectAllCheckboxText = document.createElement('H2');
+    selectAllCheckboxText.innerHTML = 'Check all';
+    selectAllCheckboxText.style.display = 'inline';
+    selectAllCheckbox.addEventListener('change', function() {
+      selectAllCheckboxes(inputs, this.checked);
+    });
+
+    selectAllCheckbox.setAttribute('type', 'checkbox');
+    document.body.appendChild(selectAllCheckbox);
+    document.body.appendChild(selectAllCheckboxText);
+
+    const allResources = [];
     const selectedRessources = [];
     msg.resources.forEach(r => {
-      const section = document.createElement('H2')
-      section.innerHTML = r.section
-      document.body.appendChild(section)
+      const section = document.createElement('H2');
+      section.innerHTML = r.section;
+      document.body.appendChild(section);
 
       r.resources.forEach(r => {
-        allResources.push(r)
-        const icon = document.createElement('img')
+        allResources.push(r);
+        const icon = document.createElement('img');
         icon.src = r.iconUrl;
-        icon.style.display = 'block'
-        
-        const item = document.createElement('H4')
-        item.innerHTML = r.name
-        item.appendChild(icon)
-        document.body.appendChild(item)
-        
-        const checkbox = document.createElement("INPUT");
+        icon.style.display = 'block';
+
+        const item = document.createElement('H4');
+        item.innerHTML = r.name;
+        item.appendChild(icon);
+        document.body.appendChild(item);
+
+        const checkbox = document.createElement('INPUT');
         inputs.push(checkbox);
         checkbox.addEventListener('change', function() {
-          if(this.checked){
+          if (this.checked) {
             counter += 1;
             selectedRessources.push(r);
-          }
-          else{
+          } else {
             counter -= 1;
             const index = selectedRessources.indexOf(r);
             selectedRessources.splice(index, 1);
           }
-          checkBoxCounter.innerHTML ='Nombre de fichiers: ' +  counter;
+          checkBoxCounter.innerHTML = 'Nombre de fichiers: ' + counter;
           console.log(selectedRessources.length);
-        })
-        checkbox.setAttribute("type", "checkbox");
+        });
+        checkbox.setAttribute('type', 'checkbox');
         document.body.appendChild(checkbox);
-        
-        const button = document.createElement('BUTTON')
-        button.innerHTML = 'download'
+
+        const button = document.createElement('BUTTON');
+        button.innerHTML = 'download';
         button.addEventListener('click', () => {
           chrome.runtime.sendMessage({
             recipient: 'background',
@@ -71,14 +70,14 @@ chrome.runtime.onMessage.addListener(msg => {
             resource: r,
             courseName: msg.courseName,
             extension: '',
-          })
-        })
-        document.body.appendChild(button)
-      })
-    })
+          });
+        });
+        document.body.appendChild(button);
+      });
+    });
 
-    const downloadAllButton = document.createElement('BUTTON')
-    downloadAllButton.innerHTML = 'download all'
+    const downloadAllButton = document.createElement('BUTTON');
+    downloadAllButton.innerHTML = 'download all';
     downloadAllButton.addEventListener('click', () => {
       chrome.runtime.sendMessage({
         recipient: 'background',
@@ -86,14 +85,14 @@ chrome.runtime.onMessage.addListener(msg => {
         resources: allResources,
         courseName: msg.courseName,
         extension: '',
-      })
-    })
-    document.body.appendChild(document.createElement('br'))
-    document.body.appendChild(document.createElement('br'))
-    document.body.appendChild(downloadAllButton)
+      });
+    });
+    document.body.appendChild(document.createElement('br'));
+    document.body.appendChild(document.createElement('br'));
+    document.body.appendChild(downloadAllButton);
 
-    const downloadSelectedButton = document.createElement('BUTTON')
-    downloadSelectedButton.innerHTML = 'download selected'
+    const downloadSelectedButton = document.createElement('BUTTON');
+    downloadSelectedButton.innerHTML = 'download selected';
     downloadSelectedButton.addEventListener('click', () => {
       chrome.runtime.sendMessage({
         recipient: 'background',
@@ -101,20 +100,19 @@ chrome.runtime.onMessage.addListener(msg => {
         resources: selectedRessources,
         courseName: msg.courseName,
         extension: '',
-      })
-    })
+      });
+    });
 
-    document.body.appendChild(document.createElement('br'))
-    document.body.appendChild(downloadSelectedButton)
-    
+    document.body.appendChild(document.createElement('br'));
+    document.body.appendChild(downloadSelectedButton);
   }
-})
+});
 
-function selectAllCheckboxes (inputs, isChecked) {
-  inputs.forEach((element) => {
-    if(element.checked != isChecked){
+function selectAllCheckboxes(inputs, isChecked) {
+  inputs.forEach(element => {
+    if (element.checked != isChecked) {
       element.checked = isChecked;
       element.dispatchEvent(new Event('change'));
     }
-  })
+  });
 }
